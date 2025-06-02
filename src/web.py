4775c095902cbd75c17387e1ac79c40b5e5358b0
ddorn <diego.dorn@free.fr>
2025-06-02 13:04:03 +0200

    feat: WIP: Add a form to prompt Configs
    * Introduce `PydanticForm` class for improved form rendering and validation of Pydantic models.
    * Refactor UI components in `web.py` to utilize the new form handling, enhancing user experience.
    * Load environment variables using `load_dotenv()` for better configuration management.
    * Update sidebar elements and improve the structure of the source selection process.
    * Remove deprecated code related to embedding and database loading, streamlining the application flow.

--- src/web.py
+++ src/web.py
@@ -1,19 +1,26 @@
-import asyncio
 import json
+import os
 import re
+import time
 from collections import defaultdict
+from pathlib import Path
 
+from openai import BaseModel
+from pydantic import Field
+from pydantic_core import PydanticUndefinedType
 import streamlit as st
 import streamlit.components.v1 as components
-from anyio import Path
-from litellm import batch_completion
-from millify import millify
+from dotenv import load_dotenv
 
-from config import load_config
+from config import setup_config
 from constants import SYNC_PATTERN
 from core_types import Asset, AssetType
-from storage import DATABASES, Database, set_db
-from strategies.embed_chunks import EmbedChunksStrategy
+from search import DocSearchResult, SearchEngine
+from storage import Database
+from sources import BUILT_IN_SOURCES
+import streamlit_react_jsonschema as srj
+
+load_dotenv()
 
 st.set_page_config(
     page_title="The Deep Search",
@@ -22,235 +29,169 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-mark_linker = components.declare_component(
-    "mark_linker",
-    path="src/streamlit-components/mark_linker/mark_linker/frontend/build",
-)
 
-# Load config
-config_path = st.sidebar.text_input("Config path", value="./data/config-simple.yaml")
-config = load_config(Path(config_path))
+debug = st.sidebar.checkbox("Debug", value=False)
 
-# Load database
-DATABASES.clear()
-db = set_db("default", Database(config.storage_path / "db.sqlite", config=config))
+@st.cache_resource
+def get_sources():
+    return []
 
-# Load embeddings
-embeddings, chunk_to_idx = db.load_embeddings()
-idx_to_chunk = {v: k for k, v in chunk_to_idx.items()}
+sources = get_sources()
 
 
-@st.cache_data
-def embed(text: str):
-    embedding = asyncio.run(EmbedChunksStrategy(None).embed_texts([text]))[0]
-    return embedding
+class PydanticForm[T: type[BaseModel]]:
+    def __init__(self, model: type[T]):
+        self.model = model
+        self.json_schema = model.model_json_schema()
+        self.defs = self.json_schema.get("$defs", {})
 
+    def render_field(self, schema: dict, key: str, field_name: str):
+        schema = self.unwrap_ref(schema)
 
-with st.sidebar:
-    st.write(f"Loaded {len(embeddings)} embeddings.")
-    st.write(config.model_dump())
+        field_type = schema.get("type")
+        field_format = schema.get("format")
+        default = schema.get("default")
 
-query = st.text_input("Query", value="delete")
-nb_results = st.slider("Number of results", 1, 100, 5)
-debugging = st.checkbox("Debug mode", value=True)
-
-
-def show_results(top_chunks):
-    st.header("Top results")
-
-    doc_to_chunks = defaultdict(list)
-
-    # Group by document_id
-    for chunk in top_chunks:
-        doc_to_chunks[chunk.document_id].append(chunk)
-
-    # Sort by document_order for each group
-    for doc_id in doc_to_chunks:
-        doc_to_chunks[doc_id].sort(key=lambda chunk: chunk.document_order)
-
-    for doc_id, chunks in doc_to_chunks.items():
-        doc = documents[doc_id]
-
-        title_col, button_col = st.columns([8, 1])
-        title_col.markdown(f"### *{doc.source_id}* {doc.title}")
-
-        PROMPT = f"""
-            Extract the most relevant excerpt (10-20 words) from the chunk of text with respect to the below query.
-
-QUERY: {query}
-
-Your response should:
-- Contain only the most relevant excerpt from the text
-- Be the most related to the above query
-- Include at least 10 words.
-- Include no additional text, introductions, or explanations
-- Preserve the exact wording from the original text
-"""
-
-        responses = batch_completion(
-            model="groq/llama-3.3-70b-versatile",
-            messages=[
-                [
-                    dict(role="system", content=PROMPT),
-                    dict(role="user", content=f"CHUNK: {chunk.content}\n"),
-                ]
-                for chunk in chunks
-            ],
+        kwargs = dict(
+            value=default,
+            key=key,
+            label_visibility="collapsed",
+            label=key,
         )
 
-        for chunk, response in zip(chunks, responses):
-            st.write(chunk.content)
-            st.write(response.choices[0].message.content)
-
-
-def show_selected_chunk(selected_chunk, assets, documents, chunks):
-    chunk = chunks[selected_chunk]
-    doc_assets = assets[chunk.document_id]
-    doc = documents[chunk.document_id]
-
-    # sync_ids = SYNC_PATTERN.findall(chunk.content)
-    # st.write(sync_ids)
-    if doc.url is not None:
-        st.link_button(
-            label="Open",
-            url=doc.url,
-            type="secondary",
-            icon=":material/open_in_new:",
-        )
-
-    # pick the type to display
-    type_ = st.radio("Type to show", list(doc_assets.keys()), index=0, horizontal=True)
-    assets_of_type = doc_assets[type_]
-
-    for asset in assets_of_type:
-        asset_content = asset.content if asset.content is not None else asset.path.read_text()
-
-        if asset.type == AssetType.SYNCED_TEXT_FILE:
-            st.markdown(f"### {asset.path}")
-
-            mark = re.search(SYNC_PATTERN, chunk.content).group("id")
-            mark_linker(markdown=asset_content, highlighted_mark=mark)
-
-        elif asset.type == AssetType.STRUCTURE:
-            st.markdown(f"### {asset.path}")
-            st.json(json.loads(asset_content))
-
-        elif asset.path is not None:
-            # For .md and .txt files, we can use .markdown, for others we can use .code
-            if asset.path.suffix in [".md", ".txt"]:
-                st.markdown(f"### {asset.path}")
-                st.markdown(asset_content)
+        if field_type == "string":
+            if field_format == "date-time":
+                return st.date_input(**kwargs)
+            elif field_format == "multiline":
+                return st.text_area(**kwargs)
             else:
-                st.markdown(f"### {asset.path}")
-                st.code(asset_content)
-
-        elif asset.type == AssetType.CHUNK_ID:
-            chunk = db.get_chunks([int(asset_content)])[0]
-            st.markdown(f"#### Chunk {chunk.document_order}")
-            st.code(chunk.content)
+                return st.text_input(**kwargs)
+        elif field_type == "number":
+            return st.number_input(**kwargs)
+        elif field_type == "object":
+            return self.render_object(schema, key=key)
+        elif field_type == "null":
+            st.write(f"{self.nice_title(schema, field_name)} is null.")
+            return None
+        elif "anyOf" in schema:
+            return self.render_any_of(schema, key=key, field_name=field_name)
         else:
-            st.markdown(f"### Asset {asset.id}")
-            st.code(asset_content)
+            st.error(f"Unsupported field type: {field_type}")
+            return None
 
-            # path = asset.path
-            # st.markdown(f"### {path}")
-            # # For .md and .txt files, we can read the text
-            # if path.suffix in [".md", ".txt"]:
-            #     st.markdown(asset_content)
-            # else:
-            #     st.code(asset_content)
+    def render_any_of(self, schema: dict, key: str, field_name: str):
+        schema = self.unwrap_ref(schema)
+        option_names = []
+        for i, option in enumerate(schema["anyOf"]):
+            option_names.append(self.nice_title(option, default=f"Option {i}"))
+
+        selected = st.radio(label=key, options=option_names, index=0, label_visibility="collapsed", key=key + "-select", horizontal=True)
+        selected_index = option_names.index(selected)
+        selected_option = schema["anyOf"][selected_index]
+        return self.render_field(selected_option, key=key + f"-{selected_index}", field_name=f"{field_name}-{selected}")
+
+    def render_object(self, schema: dict, key: str):
+        form_data = {}
+        for field_name, field in schema.get("properties", {}).items():
+            field = self.unwrap_ref(field)
+
+            title = self.nice_title(field, field_name)
+            description = field.get("description", "")
+
+            cols = st.columns(1 + debug)
+            if debug:
+                with cols[1]:
+                    st.write(field)
+
+            with cols[0]:
+                header = [f"**{title}**", description]
+                st.write("  \n".join(header))
+                form_data[field_name] = self.render_field(field, key=f"{key}_{field_name}", field_name=field_name)
+
+        # If object has no properties, we prompt for any json data, as text
+        if not form_data:
+            json_data = st.text_area(
+                label="JSON data",
+                placeholder="{\n  \"key\": \"value\"\n}",
+                key=key,
+            )
+
+            try:
+                form_data = json.loads(json_data)
+            except json.JSONDecodeError:
+                return {}
+
+        return form_data
+
+    def render(self, key: str) -> T | None:
+        """Render a form for a Pydantic model."""
+        form_data = self.render_object(self.json_schema, key=key + "_root")
+
+        submit_button = st.button(label="Submit", key=key + "_submit")
+
+        if submit_button:
+            # Validate the form data against the model
+            try:
+                validated_data = self.model.model_validate(form_data)
+                st.success("Form submitted successfully!")
+                return validated_data
+            except Exception as e:
+                st.error(f"Error: {e}")
+                return None
+        return None
 
 
-if query:
-    embedding = embed(query)
+    def unwrap_ref(self, schema: dict):
+        """Get the reference from the schema."""
+        if "$ref" in schema:
+            ref_name = schema["$ref"].split("/")[-1]
+            return self.defs[ref_name]
+        return schema
 
-    distances = embeddings @ embedding
-    top_n = distances.argsort()[-nb_results:][::-1]
-    top_chunks_ids = [idx_to_chunk[i] for i in top_n]
-    top_chunks = db.get_chunks(top_chunks_ids)
-    chunks = {chunk.id: chunk for chunk in top_chunks}
+    def nice_title(self, schema: dict, field_name: str | None = None, default: str = "Untitled"):
+        """Get the title from the schema."""
+        schema = self.unwrap_ref(schema)
 
-    if len(chunks) == 0:
-        st.text("No chunks found")
-        st.stop()
+        if "title" in schema:
+            return schema["title"]
 
-    docs_ids = set(chunk.document_id for chunk in top_chunks)
+        if field_name is not None:
+            return field_name.replace("_", " ").capitalize()
 
-    documents = {doc_id: db.get_document(doc_id) for doc_id in docs_ids}
-    assets: dict[int, dict[str, list[Asset]]] = {}
-    for doc_id in docs_ids:
-        assets[doc_id] = defaultdict(list)
-        for asset in db.get_assets_for_document(doc_id):
-            assets[doc_id][asset.type].append(asset)
-
-    st.write(
-        f"Found **{len(chunks)} chunks** associated with **{len(documents)} documents** and **{len(assets)} assets**"
-    )
-
-    selected_chunk = st.session_state.get("selected_chunk", top_chunks[0].id)
-    # If the selected chunk from the last query does not exist, we take a new one.
-    if selected_chunk not in chunks:
-        selected_chunk = top_chunks[0].id
-
-    result_col, doc_col = st.columns([1, 1])
-
-    with result_col:
-        show_results(top_chunks)
-
-    with doc_col:
-        show_selected_chunk(selected_chunk, assets, documents, chunks)
+        if "format" in schema:
+            return schema["format"].capitalize()
+        if "type" in schema:
+            return schema["type"].capitalize()
+        return default
 
 
-st.header("Statistics")
 
-tabs = st.tabs(["Tasks", "Chunks"])
+if debug:
+    for source_name, source_class in BUILT_IN_SOURCES.items():
+        config_class: type[BaseModel] = source_class.CONFIG_TYPE
+        st.subheader(source_name)
+        PydanticForm(config_class).render(source_name)
 
-with tabs[0]:
-    # Tasks per strategy (pending/in_progress/done)
-    db.cursor.execute(
-        """
-        SELECT COUNT(*), strategy, status
-        FROM tasks
-        GROUP BY strategy, status
-    """
-    )
-    rows = db.cursor.fetchall()
-    st.write("### Number of tasks per strategy and status")
+display_names = [source.DISPLAY_NAME for source in BUILT_IN_SOURCES.values()]
+descriptions = [source.DESCRIPTION for source in BUILT_IN_SOURCES.values()]
 
-    # {strategy: {status: count}}
-    stats = defaultdict(lambda: defaultdict(int))
-    for row in rows:
-        stats[row[1]][row[2]] += row[0]
+st.title("New Source Wizard")
+new_source = st.radio(
+    "New source",
+    options=display_names,
+    captions=descriptions,
+    index=None,
+)
+if new_source is None:
+    st.stop()
 
-    text = ""
-    for strategy, status_counts in stats.items():
-        # - strategy (red-pending, green-done, blue-in_progress)
-        text += f"- {strategy} (:red[{status_counts['pending']}], :green[{status_counts['done']}], :blue[{status_counts['in_progress']}])\n"
-    st.markdown(text)
-
-# Show numbers of chunks per document
-with tabs[1]:
-    # Number of words in all chunks
-    db.cursor.execute("SELECT content FROM chunks")
-    word_count = sum(len(chunk["content"].split()) for chunk in db.cursor.fetchall())
-    st.metric("Number of words in all chunks", millify(word_count, precision=1))
-
-    # Number of characters in all chunks
-    db.cursor.execute("SELECT SUM(LENGTH(content)) FROM chunks")
-    rows = db.cursor.fetchall()
-    st.metric("Number of characters in all chunks", millify(rows[0][0], precision=1))
-
-    db.cursor.execute(
-        """
-        SELECT COUNT(*), document_id
-        FROM chunks
-        GROUP BY document_id
-        """
-    )
-    rows = db.cursor.fetchall()
-    st.write("### Number of chunks per document")
-    stats = [{"document_id": row[1], "count": row[0]} for row in rows]
-    stats.sort(key=lambda x: x["count"], reverse=True)
-    for stat in stats:
-        doc = db.get_document(stat["document_id"])
-        st.markdown(f"Document {doc.source_id} ({doc.title}): {stat['count']} chunks")
+st.header(f"⚙️ {new_source}")
+source_class = next(
+    source for source in BUILT_IN_SOURCES.values() if source.DISPLAY_NAME == new_source
+)
+source_config_class = source_class.CONFIG_TYPE
+with st.container(border=True):
+    config = PydanticForm(source_config_class).render(new_source)
+if config:
+    st.write("Config:")
+    st.code(config.model_dump_json())
